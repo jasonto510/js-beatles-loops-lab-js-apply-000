7c2151699c0233c3158186a918e44af96668865d
Jason To <jasto@ucdavis.edu>
2018-10-15 19:22:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
   var musiciansPlusInstruments = []
   for (var i = 0; i < arrayMusicians.length; i ++){
-    var result = (`${arrayMusicians[i]} plays  ${arrayInstruments[i]}`)
+    var result = (`${arrayMusicians[i]} plays ${arrayInstruments[i]}`)
     musiciansPlusInstruments.push(result)
   }
   return musiciansPlusInstruments
