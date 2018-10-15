16814955c099e8890fd1f28f268ecbfe02539ccb
Jason To <jasto@ucdavis.edu>
2018-10-15 19:19:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+  var musiciansPlusInstruments = []
+  for (var i = 0; i < arrayMusicians.length; i ++){
+    musiciansPlusInstruments.push(arrayMusicians[i] + arrayInstruments[i])
+  }
+  return musiciansPlusInstruments
+}
